6d8a7176dbb36e156d8e7cfa48d219571d1f8f43
KyriakosDiplaros <p2500684@my365.dmu.ac.uk>
2021-05-13 18:13:59 +0100

    Added Test staff and class

---
+++ Testing1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class TestStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaffFinder AnStaffFinder = new clsStaffFinder();
+            Assert.IsNotNull(AnStaffFinder);
+        }
+    }
+}
