4dbc1bdb80512d4210aeb565f3aba858b5409eff
KyriakosDiplaros <p2500684@my365.dmu.ac.uk>
2021-05-13 23:50:15 +0100

    Added Fields and buttons and other misc

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -11,4 +11,21 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
 
     }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStaffData
+        ClassLibrary.clsFirstName AnStaffData = new ClassLibrary.clsFirstName();
+        //capture the staff id
+        AnStaffData.FirstName = txtStaffID.Text;
+        //store the address in the session object
+        Session["AnStaffData"] = AnStaffData;
+        //navigate to the viewer page
+        Response.Redirect("StaffViewer.aspx");
+    }
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
 }
