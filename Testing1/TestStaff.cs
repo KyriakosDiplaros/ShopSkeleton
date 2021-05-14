5033912bf044bcaf917f326a5df21ea89d88d841
KyriakosDiplaros <p2500684@my365.dmu.ac.uk>
2021-05-14 04:29:16 +0100

    Added find method

--- Testing1/TestStaff.cs
+++ Testing1/TestStaff.cs
@@ -7,28 +7,28 @@ namespace Testing1
     [TestClass]
     public class TestStaff
     {
-        
-        
+
+
 
         [TestMethod]
         public void InstanceOK()
         {
-            //create an instance of the class we want to create
-            clsStaffData anStaffID = new clsStaffData();
-            //test to see that it exists
-            Assert.IsNotNull(anStaffID);
+
+            clsStaffData AStaffID = new clsStaffData();
+
+            Assert.IsNotNull(AStaffID);
         }
 
         [TestMethod]
         public void FirstNamePropertyOK()
         {
-            //create an instance of the class we want to create
+
             clsStaffData AnFirstName = new clsStaffData();
-            //create some test data to assign to the property
+
             String TestData = "Kyriakos";
-            //assign the data to the property
+
             AnFirstName.FirstName = TestData;
-            //test to see that the two values are the same
+
             Assert.AreEqual(AnFirstName.FirstName, TestData);
 
         }
@@ -36,41 +36,41 @@ namespace Testing1
         [TestMethod]
         public void SecondNamePropertyOK()
         {
-            //create an instance of the class we want to create
+
             clsStaffData AStaff = new clsStaffData();
-            //create some test data to assign to the property
+
             string TestData = "Diplaros";
-            //assign the data to the property
+
             AStaff.LastName = TestData;
-            //test to see that the two values are the same
+
             Assert.AreEqual(AStaff.LastName, TestData);
         }
 
         [TestMethod]
         public void DepartmentPropertyOK()
         {
-            //creaet an instance of the class we want to create
+
             clsStaffData ADepartment = new clsStaffData();
-            //create some test data to assign to the property
+
             string TestData = "Owner";
-            //assign the data to the property
+
             ADepartment.Department = TestData;
-            //test to see that the two values are the same
+
             Assert.AreEqual(ADepartment.Department, TestData);
 
         }
-        
+
 
         [TestMethod]
         public void JoinDatePropertyOK()
         {
-            //create an instance of the class we want to create
+
             clsStaffData AJoinDate = new clsStaffData();
-            //create some test data to assign to the property
+
             DateTime TestData = DateTime.Now.Date;
-            //assign the data to the property
+
             AJoinDate.JoinDate = TestData;
-            //test to see that the two values are the same
+
             Assert.AreEqual(AJoinDate.JoinDate, TestData);
         }
 
@@ -78,13 +78,13 @@ namespace Testing1
         [TestMethod]
         public void ActivePropertyOK()
         {
-            //create an instance of the class we want to create
+
             clsStaffData AStaff = new clsStaffData();
-            //create some test data to assign to the property
+
             Boolean TestData = true;
-            //assign the data to the property
+
             AStaff.Active = TestData;
-            //test to see that the two values are the same
+
             Assert.AreEqual(AStaff.Active, TestData);
         }
 
@@ -94,20 +94,20 @@ namespace Testing1
         public void StaffIDOK()
         {
             clsStaffData AStaff = new clsStaffData();
-            Int32 TestData = 1;
+            Int32 TestData = 10;
             AStaff.StaffID = TestData;
             Assert.AreEqual(AStaff.StaffID, TestData);
 
         }
         public void StaffStatusTrue()
-        { 
-        //create an instance of the class we want to create
-        clsStaffData AnStaffData = new clsStaffData();
-            //create some test data
+        {
+
+            clsStaffData AnStaffData = new clsStaffData();
+
             Boolean TestData = true;
-            //assign data to the property
+
             AnStaffData.Active = TestData;
-            //test to see that the two values are the same
+
             Assert.AreEqual(AnStaffData.Active, TestData);
         }
 
@@ -115,18 +115,79 @@ namespace Testing1
 
         public void FindMethodOK()
         {
-            //create an instance of the class we want to create
+
             clsStaffData AStaff = new clsStaffData();
-            //boolean variable to store the results of the validation
+
             Boolean found = false;
-            Int32 StaffID = 1;
-            //invoke the method
+            Int32 StaffID = 10;
+            
             found = AStaff.Find(StaffID);
-            //test to see if the result is true
+
             Assert.IsTrue(found);
         }
     }
-
-   
-
+    [TestMethod]
+    public void TestStaffIDFound()
+    {
+        
+        clsStaffData AStaff = new clsStaffData();
+        
+        Boolean Found = false;
+      
+        Boolean OK = true;
+        
+        Int32 StaffID = 10;
+        
+        Found = AStaff.Find(StaffID);
+        
+        if (AStaff.StaffID != 10)
+        {
+            OK = false;
+        }
+        //test to see if result is true
+        Assert.IsTrue(OK);
+    }
 }
+
+[TestMethod]
+public void FirstNamePropertyOK()
+{
+    clsStaffData AStaff = new clsStaffData();
+    string TestData = "Kyriakos";
+    AStaff.FirstName = TestData;
+    Assert.AreEqual(AStaff.FirstName, TestData);
+}
+
+[TestMethod]
+public void LastNamePropertyOK()
+{
+    clsStaffData AStaff = new clsStaffData();
+    string TestData = "Diplaros";
+    AStaff.LastName = TestData;
+    Assert.AreEqual(AStaff.LastName, TestData);
+}
+
+[TestMethod]
+public void DepartmentPropertyOK()
+{
+    clsStaffData AStaff = enw clsStaffData
+        string TestData = "Owner";
+    AStaff.Department = TestData;
+    Assert.AreEqual(AStaff.Department, TestData);
+}
+
+[TestMethod]
+public void TestJoinDateFound()
+{
+    clsStaffData AStaff = new clsStaffData();
+    Boolean Found = false;
+    Boolean OK = true;
+    Int32 StaffID = 10;
+    Found = AStaff.Find(StaffID);
+    if (AStaff.JoinDate != Convert.ToDateTime("03/05/2019"))
+    {
+        OK = false;
+    }
+    Assert.IsTrue(OK);
+}
+
