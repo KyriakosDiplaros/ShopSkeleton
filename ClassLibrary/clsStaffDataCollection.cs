2113d1fb1478ed1d26c2d34884de3c2b4132d15c
KyriakosDiplaros <p2500684@my365.dmu.ac.uk>
2021-05-14 08:06:13 +0100

    Added more functions

---
+++ ClassLibrary/clsStaffDataCollection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ClassLibrary;
+using System.Collections.Generic;
+
+
+
+namespace ClassLibrary
+{
+
+    public clsStaffDataCollection()
+    {
+        Int32 Index = 0;
+        Int32 RecordCount = 0;
+        clsDataConnection DB = new clsDataConnection();
+        DB.Execute("sproc_tblStaffData_SelectAll");
+        RecordCount = DB.Count;
+        while (Index < RecordCount )
+        {
+            AStaff.Status = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+            AStaff.FirstName = Convert.ToString(DB.DataTable.Rows[Index]["FirstName"]);
+            AStaff.LastName = Convert.ToString(DB.DataTable.Rows[Index]["LastName"]);
+            AStaff.Department = Convert.ToString(DB.DataTable.Rows[Index]["Department"]);
+            AStaff.StaffID = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffID"]);
+            AStaff.JoinDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["JoinDate"]);
+        }
+    }
+
+    public class clsStaffDataCollection
+    {
+        List<clsStaffData> mStaffData = new List<clsStaffData>();   
+        {
+public List<clsStaffData> StaffList { get; set; }
+    }
+    }
+public List<clsStaffData> StaffList
+{
+    get
+    {
+        return mStaffData;
+    }
+    set
+    {
+        mStaffData = value;
+    }
+
+public int Count
+{
+    get
+    {
+        return mStaffData.Count;
+    }
+    set
+    {
+
+    }
+}
---
+++ Testing1/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStaffDataCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+
+        
+            clsStaffDataCollection AllStaffs = new clsStaffDataCollection();
+        Assert.IsNotNull(AllStaffs);
+        
+        }
+    }
+}
+
+
+[TestMethod]
+public void StaffListOK()
+{
+    clsStaffDataCollection AllStaffs = new clsStaffDataCollection();
+    List<clsStaffData> TestList = new List<clsStaffData>();
+    clsStaffData TestItem = new clsStaffData();
+    TestItem.StaffID = 10;
+    TestItem.FirstName = "Kyriakos";
+    TestItem.LastName = "Diplaros";
+    TestItem.Department = "Owner";
+    TestItem.JoinDate = DateTime.Now.Date;
+    TestList.Add(TestItem);
+    AllStaffs.StaffList = TestList;
+    Assert.AreEqual(AllStaffs.StaffList, TestList);
+}
+
+[TestMethod]
+public void ListAndCountOk()
+{
+    clsStaffDataCollection AllStaffs = new clsStaffDataCollection();
+    List<clsStaffData> TestList = new List<clsStaffData>();
+    clsStaffData TestItem = new clsStaffData();
+    TestItem.StaffID = 10;
+    TestItem.FirstName = "Kyriakos";
+    TestItem.LastName = "Diplaros";
+    TestItem.Department = "Owner";
+    TestItem.JoinDate = DateTime.Now.Date;
+    TestList.Add(TestItem);
+    AllStaffs.StaffList = TestList;
+    Assert.AreEqual(AllStaffData.Count, TestList.Count);
+}
+
+[TestMethod]
+public void CountPropertyOK()
+{
+    clsStaffDataCollection AllStaffs = new clsStaffDataCollection();
+    Int32 SomeCount = 10;
+    AllStaffs.Count = SomeCount;
+    Assert.AreEqual(AllStaffs.Count, SomeCount);
+}
+
+[TestMethod]
+public void TwoRecordsPresent()
+{
+    clsStaffDataCollection AllStaffs = new clsStaffDataCollection();
+    Assert.AreEqual(AllAddresses.Count, 2);
+}
